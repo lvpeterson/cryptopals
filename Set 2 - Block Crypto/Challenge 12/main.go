7dfcea36b442bc7f5a2e353498e7d162e08b20b5
Logan Peterson <lv.peterson@yahoo.com>
2017-12-08 14:49:20 -0800

    starting challenge

--- Set 2 - Block Crypto/Challenge 12/main.go
+++ Set 2 - Block Crypto/Challenge 12/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/lvpeterson/cryptopals/crypt"
@@ -9,6 +10,17 @@ import (
 func main() {
 	aesKeySize := 16
 	aesKey := crypt.GenerateBytes(aesKeySize)
-	fmt.Println(aesKey)
 
+	b64string := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg" +
+		"aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq" +
+		"dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg" +
+		"YnkK"
+	myString := []byte("AAAAAAAAAAAAAAABBBBBBBBBBBBBCCCCCCCCCCCCCCCC")
+	decodedString, err := base64.StdEncoding.DecodeString(b64string)
+	crypt.Check(err)
+
+	combinedStr := append(myString, decodedString...)
+	fmt.Println(combinedStr)
+
+	fmt.Println(aesKey)
 }
