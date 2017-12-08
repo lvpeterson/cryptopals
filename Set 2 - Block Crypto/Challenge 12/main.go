e4f17890dd03971acd38d12af8ede6dcc36a000d
Logan Peterson <lv.peterson@yahoo.com>
2017-12-08 13:34:18 -0800

    edits

--- Set 2 - Block Crypto/Challenge 12/main.go
+++ Set 2 - Block Crypto/Challenge 12/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/lvpeterson/cryptopals/cryptoHelp"
+	"github.com/lvpeterson/cryptopals/crypt"
 )
 
 func main() {
 	aesKeySize := 16
 	aesKey := crypt.generateBytes(aesKeySize)
 	fmt.Println(aesKey)
+
 }
